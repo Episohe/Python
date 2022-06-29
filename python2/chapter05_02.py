3df0ce294ee89e36065eb2d6c55bb2ea5e92855e
episohe <maybe_be@naver.com>
2022-06-29 16:49:52 +0900

    :sparkles: chapter05_03

--- python2/chapter05_02.py
+++ python2/chapter05_02.py
@@ -41,7 +41,7 @@ print('--------------------------------------------------------')
 # Closure(클로저) 사용 이유 "상태를 기억한다."
 # 서버 프로그래밍 -> 동시성(Concurrency)제어 -> 메모리 공간에 여러 지원이 접근 -> 교착 상태(Dead Lock)
 # 메모리를 공유하지 않고 메세지 전달로 처리하기 위한 ->  Erlang
-# 클로저는 공유하되 변경되지 않는(Immutable, Read Only) 전문적으로 사용 -> 함수형 프로그래밍
+# 클로저는 공유하되 변경 되지 않는(Immutable, Read Only) 전문적으로 사용 -> 함수형 프로그래밍
 # 클로저는 불변자료구조 및 atom, STM -> 멀티스레드(coreoutine) 프로그래밍에 강점
 
 a = 100
@@ -74,4 +74,5 @@ print(averager_cls(30))
 print(averager_cls(50))
 print(averager_cls(70))
 print(averager_cls(193))
+
 

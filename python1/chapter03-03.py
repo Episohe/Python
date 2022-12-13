6cce82e7874d12bc76ff21d9a74fa6764b3024c0
episohe <maybe_be@naver.com>
2022-12-13 11:10:03 +0900

    :star: chapter03-3 리스트

---
+++ python1/chapter03-03.py
@@ -0,0 +1,109 @@
+# Chapter03-3
+# 파이썬 리스트
+# 자료구조에서 중요
+# 파이썬 배열 제공X
+# 리스트 자료형(순서O, 중복O, 수정O, 삭제O)
+
+# 선언
+a = []
+b = list()
+c = [70, 75, 80, 85]
+d = [1000, 10000, 'Ace', 'Base', 'Captine']
+e = [1000, 10000, ['Ace', 'Base', 'Captine']]
+f = [21.42, 'foobar', 3, 4, 'bark', False, 3.14159]
+
+# 인덱싱
+print('>>>>>>')
+print('d - ', type(d), d)
+print('d - ', d[1])
+print('d - ', d[0] + d[1] + d[1])
+print('d - ', d[-1])
+print('e - ', e[-1][1])
+print('e - ', list(e[-1][1]))
+
+# 슬라이싱
+print('>>>>>>')
+print('d - ', d[0:3])
+print('d - ', d[2:])
+print('e - ', e[2][1:3])
+
+# 리스트 연산
+print('>>>>>>리스트 연산')
+print('c + d - ', c + d)
+print('c * 3 - ', c * 3)
+# print("c[0] + 'hi' - ", c[0] + 'hi')
+print("'Test' + c[0] - ", 'Test' + str(c[0]))
+
+# 값 비교
+print('>>>>>>값 비교')
+print(c == c[:3] + c[3:])
+
+# 같은 id 값
+temp = c
+print(c == temp)
+
+# 리스트 수정, 삭제
+print('>>>>>>')
+c[0] = 4
+print('c - ', c)
+c[1:2] = ['a', 'b', 'c']  # [['a', 'b', 'c']]
+print('c - ', c)
+c[1] = ['a', 'b', 'c']
+print('c - ', c)
+c[1:3] = []
+print('c - ', c)
+del c[3]
+print('c - ', c)
+
+# 리스트 함수
+a = [5, 2, 3, 1, 4]
+
+print('>>>>> 리스트함수')
+print('a - ', a)
+a.append(6)
+print('a - ', a)
+a.sort()
+print('a - ', a)
+a.reverse()
+print('a - ', a)
+print('a - ', a.index(5))
+a.insert(2, 7)
+print('a - ', a)
+a.reverse()
+a.remove(1)
+print('a - ', a)
+print('a - ', a.pop())
+print('a - ', a.pop())
+print('a - ', a)
+print('a - ', a.count(4))
+ex = [8, 9]
+a.extend(ex)
+print('a - ', a)
+print('>>>>>>>리스트 함수 끝')
+# 삭제 remove, pop, del
+
+# 반복문 활용
+while a:
+    l = a.pop()
+    print(2 == l)
+
+# del
+numbers = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29]
+print(numbers)
+del numbers[0:4]
+print(numbers)
+
+# clear()
+numbers = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29]
+numbers.clear()
+print(numbers)
+
+# remove()
+numbers = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29]
+numbers.remove(13)
+print(numbers)
+
+# pop()
+numbers = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29]
+print(numbers.pop())
+print(numbers.pop(0))
